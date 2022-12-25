6261cf0a2790754b2f2d2acc90e5926c003a22b0
Alexander Leishman <alex@river.com>
2022-12-25 14:24:37 -0500

    start framework for basic event validation logic

--- test/nostrex/events/events_test.exs
+++ test/nostrex/events/events_test.exs
@@ -3,18 +3,23 @@ defmodule Nostrex.EventsTest do
   alias Nostrex.Events
   alias Nostrex.Events.Event
 
+  defp sample_event_params() do
+    %{
+      id: "dc90c95f09947507c1044e8f48bcf6350aa6bff1507dd4acfc755b9239b5c962",
+      pubkey: "3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d",
+      created_at: DateTime.from_unix!(1671994854),#DateTime.utc_now(),
+      kind: 1,
+      content: "jet fuel can't melt steel beams",
+      sig: "230e9d8f0ddaf7eb70b5f7741ccfa37e87a455c9a469282e3464e2052d3192cd63a167e196e381ef9d7e69e9ea43af2443b839974dc85d8aaab9efe1d9296524"
+    }
+  end
+
   test "create new event and persist" do
     original_event_count = Repo.aggregate(Event, :count)
     assert original_event_count == 0
 
-    {:ok, event} = Events.create_event(%{
-      id: "dc90c95f09947507c1044e8f48bcf6350aa6bff1507dd4acfc755b9239b5c962",
-      pubkey: "3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d",
-      created_at: DateTime.utc_now(),
-      kind: 1,
-      content: "jet fuel can't melt steel beams",
-      sig: "230e9d8f0ddaf7eb70b5f7741ccfa37e87a455c9a469282e3464e2052d3192cd63a167e196e381ef9d7e69e9ea43af2443b839974dc85d8aaab9efe1d9296524"
-    })
+    event_params = sample_event_params()
+    {:ok, event} = Events.create_event(event_params)
 
     # assert only one event created
     new_event_count = Repo.aggregate(Event, :count)
@@ -23,5 +28,37 @@ defmodule Nostrex.EventsTest do
     # assert id persisted
     saved_event = Repo.one(Event)
     assert saved_event.id == event.id
+  end
+
+  test "ensure validation of proper field lengths and formatting" do
+    
+  end
+
+  test "ensure valid created_at timestamp" do
+    
+  end
+
+  test "ensure kind value is valid" do
+    
+  end
+
+  test "ensure invalid signature events don't get persisted" do
+    
+  end
+
+  # TODO remove DB calls from these functions
+  test "ensure event serialization happens correctly" do
+    event_params = sample_event_params()
+    {:ok, event} = Events.create_event(event_params)
+
+    {:ok, serialized_event} = Events.serialize_event(event)
+    assert serialized_event == "[0,\"3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d\",1671994854,1,[],\"jet fuel can't melt steel beams\"]"
+  end
+
+  test "ensure event id generation is accurate" do
+    event_params = sample_event_params()
+    {:ok, event} = Events.create_event(event_params)
+
+    IO.inspect(Events.calculate_event_id(event))
   end
 end
